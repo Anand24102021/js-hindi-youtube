0f73ecae88b58767894675c59a403879f7fcfdad
Anand24102021 <anand99992016@gmail.com>
2024-09-15 06:51:16 +0000

    dataTypes of null

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // Treat all javaScript code as  newer version
+
+//alert(3 + 3)  We are using node.js, Not browser
+
+console.log(3 +
+     3) // Code readibility shoud be high
+
+
+console.log("Hitesh");
+
+let name = "Hitesh"
+let age = 18
+let isLoggedIn = false
+     
+//number => 2 to the power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value which is a data type return object
+//undefined => Not assign a value
+//symbol => unique
+
+//Object 
+
+
+
+console.log(typeof undefined); // type is undefined
+
+console.log(typeof null); // type is object
