10c02afcbfe51824543e79991975fea941941116
Anand24102021 <anand99992016@gmail.com>
2024-09-19 04:14:02 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,12 @@
+const name = "Pawan"
+const repoCount = 50
+
+//console.log(name + repoCount + "Value"); 
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Hiteshhhc")
+
+console.log(gameName[0])
+
+
